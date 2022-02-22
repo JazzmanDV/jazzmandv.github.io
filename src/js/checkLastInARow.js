a2796407742ee127b1016773fc660fbe8000bfab
Dan <danapple2013@gmail.com>
2022-02-22 21:07:00 +0300

    Исправлены списки скиллов

--- src/js/checkLastInARow.js
+++ src/js/checkLastInARow.js
@@ -1,17 +1,16 @@
 export default function checkLastInARow() {
-    const lists = document.querySelectorAll(".inner-skills-list");
+    const lists = document.querySelectorAll(".list-for-check");
 
     for (let list of lists) {
         const listItems = list.querySelectorAll(".inner-skills-list__item");
         const rows = new Map();
 
         for (let listItem of listItems) {
-            // Сравниваем по нижней части компонента (по скилл-бару), т.к. они выровнены в линию
-            const skillLevel = listItem.querySelector(".bullet-list");
-            if (!rows.has(skillLevel.offsetTop)) {
-                rows.set(skillLevel.offsetTop, []);
+            const distanceFromTopDocumentToBottomElement = listItem.getBoundingClientRect().bottom + window.scrollY;
+            if (!rows.has(distanceFromTopDocumentToBottomElement)) {
+                rows.set(distanceFromTopDocumentToBottomElement, []);
             }
-            rows.get(skillLevel.offsetTop).push(listItem);
+            rows.get(distanceFromTopDocumentToBottomElement).push(listItem);
         }
 
         for (let row of rows.values()) {
